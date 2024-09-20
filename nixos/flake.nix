1739c7fd50416c73270fb6f6b82931c4455f9d7e
Fabián Heredia Montiel <fabianhjr@protonmail.com>
2024-09-20 15:37:38 -0600

    sync

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -4,18 +4,32 @@
     # nixpkgs.url = "github:NixOS/nixpkgs/staging-next";
     nixpkgs.url = "flake:localnixpkgs";
 
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     lix-module = {
       url = "https://git.lix.systems/lix-project/nixos-module/archive/2.91.0.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  outputs = { self, nixpkgs, lix-module, ... }@attrs: {
+  outputs = { self, nixpkgs, home-manager, lix-module, ... }@attrs: {
     nixosConfigurations.fabian-desktop = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = attrs;
       modules = [
-        lix-module.nixosModules.default
         ./configuration.nix
+
+        home-manager.nixosModules.home-manager {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+
+          # TODO replace ryan with your own username
+          home-manager.users.fabian = import ./home/fabian.nix;
+        }
+
+        lix-module.nixosModules.default
       ];
     };
   };
